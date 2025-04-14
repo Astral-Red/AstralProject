a712d27d8afa9827fd50c5167ef421e485b1fcbe
Astral-Red <mastroilbigoo54@gmail.com>
2025-04-14 10:17:17 +0200

    Add files via upload

---
+++ AstralBypss.bat
@@ -0,0 +1,102 @@
+@echo off
+chcp 65001 >nul
+cls
+
+set "InstallDir=C:\AstralBypass-Direc"
+set "Run=RunTool.bat"
+set "Txt=Instruction.txt"
+if not exist "%InstallDir%" mkdir "%InstallDir%"
+
+color 0B
+echo.
+powershell.exe -Command Write-host "─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─" -ForegroundColor White
+echo.
+echo.
+echo     ▄▄▄        ██████ ▄▄▄█████▓ ██▀███   ▄▄▄       ██▓     ▄▄▄▄ ▓██   ██▓ ██▓███   ▄▄▄        ██████   ██████ ▓█████  ██▀███     
+echo    ▒████▄    ▒██    ▒ ▓  ██▒ ▓▒▓██ ▒ ██▒▒████▄    ▓██▒    ▓█████▄▒██  ██▒▓██░  ██▒▒████▄    ▒██    ▒ ▒██    ▒ ▓█   ▀ ▓██ ▒ ██▒   
+echo    ▒██  ▀█▄  ░ ▓██▄   ▒ ▓██░ ▒░▓██ ░▄█ ▒▒██  ▀█▄  ▒██░    ▒██▒ ▄██▒██ ██░▓██░ ██▓▒▒██  ▀█▄  ░ ▓██▄   ░ ▓██▄   ▒███   ▓██ ░▄█ ▒   
+echo    ░██▄▄▄▄██   ▒   ██▒░ ▓██▓ ░ ▒██▀▀█▄  ░██▄▄▄▄██ ▒██░    ▒██░█▀  ░ ▐██▓░▒██▄█▓▒ ▒░██▄▄▄▄██   ▒   ██▒  ▒   ██▒▒▓█  ▄ ▒██▀▀█▄     
+echo     ▓█   ▓██▒▒██████▒▒  ▒██▒ ░ ░██▓ ▒██▒ ▓█   ▓██▒░██████▒░▓█  ▀█▓░ ██▒▓░▒██▒ ░  ░ ▓█   ▓██▒▒██████▒▒▒██████▒▒░▒████▒░██▓ ▒██▒   
+echo     ▒▒   ▓▒█░▒ ▒▓▒ ▒ ░  ▒ ░░   ░ ▒▓ ░▒▓░ ▒▒   ▓▒█░░ ▒░▓  ░░▒▓███▀▒ ██▒▒▒ ▒▓▒░ ░  ░ ▒▒   ▓▒█░▒ ▒▓▒ ▒ ░▒ ▒▓▒ ▒ ░░░ ▒░ ░░ ▒▓ ░▒▓░   
+echo      ▒   ▒▒ ░░ ░▒  ░ ░    ░      ░▒ ░ ▒░  ▒   ▒▒ ░░ ░ ▒  ░▒░▒   ░▓██ ░▒░ ░▒ ░       ▒   ▒▒ ░░ ░▒  ░ ░░ ░▒  ░ ░ ░ ░  ░  ░▒ ░ ▒░   
+echo      ░   ▒   ░  ░  ░    ░        ░░   ░   ░   ▒     ░ ░    ░    ░▒ ▒ ░░  ░░         ░   ▒   ░  ░  ░  ░  ░  ░     ░     ░░   ░    
+echo          ░  ░      ░              ░           ░  ░    ░  ░ ░     ░ ░                    ░  ░      ░        ░     ░  ░   ░        
+echo.                                                                 ░░ ░                                                             
+echo.
+echo.
+echo.
+echo.    
+powershell.exe -Command Write-host " ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─" -ForegroundColor White
+echo.
+echo.
+powershell.exe -Command Write-host "THIS SCRIPT IS ONLY TO BYPASS PC WITH ADMIN PASSWORD. IM NOT RESPONSABLE FOR THE USE YOU MAKE." -ForegroundColor DarkRed
+echo.                              
+powershell.exe -Command Write-host " ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─" -ForegroundColor White
+echo. 
+echo        [1] osu!                                    [2] WinRar
+echo. 
+echo.
+echo.
+echo.
+echo.
+echo        [3] Euro Truck Simulator 2                  [4] ???
+echo.
+echo.
+powershell.exe -Command Write-host " ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─" -ForegroundColor White
+echo.
+
+
+set /p Choose="Type what installation do you want to start (1,2,?,?): "
+
+setlocal enabledelayedexpansion
+set "download_url="
+set "installer_name="
+
+if "%Choose%"=="1" (
+    set "download_url=https://www.dropbox.com/scl/fi/90ilw2lhoqmm2jcklnzss/osu-install.exe?rlkey=9bduj31fr3sr1an7xidmy83qk&st=5fyp8lsz&dl=1"
+    set "installer_name=osuinstall.exe"
+) else if "%Choose%"=="2" (
+    set "download_url=https://www.dropbox.com/scl/fi/g71b1f6lred8i5bh5cg2f/winrar-x64-710it.exe?rlkey=sv4w3sr47kmk5v6lt77w6re74&st=ozg13kby&dl=1"
+    set "installer_name=winrar.exe"
+) else if "%Choose%"=="3" (
+    set "download_url=???"
+    set "installer_name=???"
+) else if "%Choose%"=="4" (
+    set "download_url=???
+    set "installer_name=????"
+) else (
+    echo Choose not valid. Exit...
+    exit /b
+)
+
+powershell -Command "& {Invoke-WebRequest -Uri '%download_url%' -OutFile '%InstallDir%\%installer_name%'}"
+
+if "%Choose%"=="2" (
+(
+echo If you have chosen the WinRAR installation, you need to create a new folder on your desktop. > "%InstallDir%\%Txt%"
+echo Rename it to "WinRAR" and select the folder you just created in the WinRAR installer. >> "%InstallDir%\%Txt%"
+echo After selecting the folder, you can proceed with the installation. >> "%InstallDir%\%Txt%"
+echo If you need help, visit my GitHub: https://github.com/astral-red/AstralProject >> "%InstallDir%\%Txt%"
+)
+)
+
+(
+echo cmd /min /C "set __COMPAT_LAYER=runasinvoker && start "" "%%1"
+) > "%InstallDir%\%Run%"
+
+echo Starting the Download...
+
+set /p run="Do you want to run the game? (S/N): "
+if /i "%run%"=="S" (
+echo.
+echo.
+   powershell.exe -Command Write-host  "To start the game just drag the installer in the directory into the file RunTool.bat"  -ForegroundColor Red
+   powershell.exe -Command Write-host  "// if you need help just use github: https://github.com/Astral-red/Support" -ForegroundColor Red
+echo.
+echo.
+    start "" "%InstallDir%\%game_exe%"
+) else (
+    echo Installation Compleate. Now you can close the CMD.
+)
+
+pause
